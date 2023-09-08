f3b32bf643cc701d289cfb514403d08817ace2de
Adrian-Marin <a18310176@live.ceti.mx>
2023-09-07 19:58:57 -0600

    Read all Data from CFDI

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,7 @@ document.getElementById("file").addEventListener("change", function (e) {
 function readXMLDataFromFile(xml){
     var xmlDoc = xml.responseXML;
 
+    // 1. Datos Globales del CFDI
     console.error("Elementos Generales");
     var comprobante = xmlDoc.querySelector("cfdi\\:Comprobante, Comprobante"); // Intenta buscar con o sin el prefijo
     var atributosComprobante = ["Version", "Fecha", "Sello", "FormaPago", "NoCertificado", "Certificado", "SubTotal", "Total", "TipoDeComprobante", "Exportacion", "MetodoPago", "LugarExpedicion"];
@@ -24,6 +25,7 @@ function readXMLDataFromFile(xml){
         alert("El elemento cfdi:Comprobante no se encontró en el documento XML.");
     }
 
+    // 2. Datos Globales del CFDI
     console.error("Información Global");
     var informacionGlobal = xmlDoc.querySelector("cfdi\\:InformacionGlobal, InformacionGlobal"); // Intenta buscar con o sin el prefijo
     var atributosInfGlob = ["Periodicidad", "Meses", "Año"];
@@ -35,6 +37,7 @@ function readXMLDataFromFile(xml){
         alert("El elemento cfdi:Emisor no se encontró en el documento XML.");
     }
 
+    // 3. Información del Emisor
     console.error("Información del Emisor");
     var emisor = xmlDoc.querySelector("cfdi\\:Emisor, Emisor"); // Intenta buscar con o sin el prefijo
     var atributosEmisor = ["Rfc", "Nombre", "RegimenFiscal"];
@@ -46,6 +49,7 @@ function readXMLDataFromFile(xml){
         alert("El elemento cfdi:Emisor no se encontró en el documento XML.");
     }
 
+    // 4. Información del Receptor
     console.error("Información del Receptor");
     var receptor = xmlDoc.querySelector("cfdi\\:Receptor, Receptor"); // Intenta buscar con o sin el prefijo
     var atributosReceptor = ["Rfc", "Nombre", "DomicilioFiscalReceptor", "UsoCFDI"];
@@ -57,4 +61,34 @@ function readXMLDataFromFile(xml){
         alert("El elemento cfdi:Receptor no se encontró en el documento XML.");
     }
 
+    // 5. Conceptos
+    console.error("Información de los Conceptos");
+    var conceptos = xmlDoc.getElementsByTagName('cfdi:Conceptos')[0];
+    var atributosConcepto = ["ClaveProdServ", "Cantidad", "ClaveUnidad", "Unidad", "Descripcion", "ValorUnitario", "Importe", "ObjetoImp"];
+    if (conceptos) {
+        var conceptoElements = conceptos.getElementsByTagName('cfdi:Concepto');
+        if(conceptoElements){
+            for (var i = 0; i < conceptoElements.length; i++) {
+                console.warn("Concepto " + (i+1) + ":");
+                var concepto = conceptoElements[i];
+                for(let i = 0; i < atributosConcepto.length; i++){
+                    console.log(atributosConcepto[i] + ": " + concepto.getAttribute(atributosConcepto[i]));
+                }
+            }
+        }
+    } else {
+        alert("No se encontraron Conceptos en la factura.");
+    }
+
+    // Timbre Fiscal Digital
+    console.error("Timbre Fiscal Digital");
+    var receptor = xmlDoc.querySelector("tfd\\:TimbreFiscalDigital, TimbreFiscalDigital"); // Intenta buscar con o sin el prefijo
+    var atributosReceptor = ["UUID", "FechaTimbrado", "RfcProvCertif", "SelloCFD", "NoCertificadoSAT", "SelloSAT"];
+    if (receptor) {
+        for(let i = 0; i < atributosReceptor.length; i++){
+            console.log(atributosReceptor[i] + ": " + receptor.getAttribute(atributosReceptor[i]));
+        }
+    } else {
+        alert("El elemento cfdi:Receptor no se encontró en el documento XML.");
+    }
 }
